03f9b8db24cb31172ad9913775ea9173983a5471
Kevin Hinton <Kevin Hinton>
2016-04-11 17:41:45 -0700

    disable image minification for quality reasons

--- Gruntfile.js
+++ Gruntfile.js
@@ -390,7 +390,7 @@ module.exports = function (grunt) {
 		// Run some tasks in parallel to speed up the build process
 		concurrent: {
 			dist: [
-				'imagemin',
+				// 'imagemin',
 				'svgmin'
 			]
 		},
